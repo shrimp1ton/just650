dd417b02039cf98e6795ebef2644d91a39a19b82
lancepr1 <108348802+lancepr1@users.noreply.github.com>
2024-08-19 21:19:36 -0400

    change case

--- api/essays.js
+++ api/essays.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const Essay = require('../models/essay');
+const Essay = require('../models/Essay');
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
